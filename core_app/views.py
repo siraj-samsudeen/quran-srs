99a4a2136f84cfdc5d39869dee9ba2444cd6d645
Siraj Samsudeen <mailsiraj@gmail.com>
2020-04-30 20:46:54 +0530

    Multiple hacks to the view

--- core_app/views.py
+++ core_app/views.py
@@ -101,6 +101,7 @@ keys_map = {
     "8.scheduled_due_date": "Due On",
     "overdue_days": "Overdue Days",
     "risk_rank": "Risk Rank",
+    "sort_order": "Sort Order",
 }
 
 keys_map_all = {
@@ -157,75 +158,78 @@ def page_due(request, student_id):
 
     pages_due = get_pages_due(student_id)
 
-    # Implement the algorithm to computer page risk
+    if pages_due:
+        # Implement the algorithm to computer page risk
 
-    df = pd.DataFrame.from_dict(pages_due, orient="index",)
-    df.drop(
-        [
-            "2.revision date",
-            "8.scheduled_due_date",
-            "mistakes",
-            "4.current_interval",
-            "5.interval_delta",
-            "6.max_interval",
-            "is_due",
-            "score_cumulative",
-        ],
-        axis=1,
-        inplace=True,
-    )
+        df = pd.DataFrame.from_dict(pages_due, orient="index",)
+        df.drop(
+            [
+                "2.revision date",
+                "8.scheduled_due_date",
+                "mistakes",
+                "4.current_interval",
+                "5.interval_delta",
+                "6.max_interval",
+                "is_due",
+                "score_cumulative",
+            ],
+            axis=1,
+            inplace=True,
+        )
 
-    df.rename(
-        columns={
-            "7.scheduled_interval": "interval",
-            "1.revision_number": "revisions",
-            "score_average": "average_score",
-            "3.score": "latest_score",
-            "page_strength": "interval_per_revision",
-            "overdue_days": "overdue_days",
-        },
-        inplace=True,
-    )
+        df.rename(
+            columns={
+                "7.scheduled_interval": "interval",
+                "1.revision_number": "revisions",
+                "score_average": "average_score",
+                "3.score": "latest_score",
+                "page_strength": "interval_per_revision",
+                "overdue_days": "overdue_days",
+            },
+            inplace=True,
+        )
 
-    cols_new_order = [
-        "interval",
-        "revisions",
-        "average_score",
-        "latest_score",
-        "interval_per_revision",
-        "overdue_days",
-    ]
-    df = df[cols_new_order]
+        cols_new_order = [
+            "interval",
+            "revisions",
+            "average_score",
+            "latest_score",
+            "interval_per_revision",
+            "overdue_days",
+        ]
+        df = df[cols_new_order]
 
-    df["interval_modified"] = np.where(df.interval >= 30, -1 * df.interval, df.interval)
-    df["rank_interval"] = df["interval_modified"].rank(pct=True, ascending=False)
-    df["rank_revisions"] = df["revisions"].rank(pct=True, ascending=False)
-    df["rank_average_score"] = df["average_score"].rank(pct=True, ascending=True)
-    df["rank_latest_score"] = df["latest_score"].rank(pct=True, ascending=True)
-    df["rank_interval_per_revision"] = df["interval_per_revision"].rank(
-        pct=True, ascending=False
-    )
-    df["rank_overdue_days"] = df["overdue_days"].rank(pct=True, ascending=False)
-    df["rank_overall"] = (
-        df.rank_interval
-        + df.rank_revisions
-        + df.rank_average_score
-        + df.rank_latest_score
-        + df.rank_interval_per_revision
-        + df.rank_overdue_days
-    )
-    # Need a unique rank for each item even if there are ties
-    df["risk_rank"] = df["rank_overall"].rank(ascending=False, method="first")
+        df["interval_modified"] = np.where(
+            df.interval >= 30, -1 * df.interval, df.interval
+        )
+        df["rank_interval"] = df["interval_modified"].rank(pct=True, ascending=False)
+        df["rank_revisions"] = df["revisions"].rank(pct=True, ascending=False)
+        df["rank_average_score"] = df["average_score"].rank(pct=True, ascending=True)
+        df["rank_latest_score"] = df["latest_score"].rank(pct=True, ascending=True)
+        df["rank_interval_per_revision"] = df["interval_per_revision"].rank(
+            pct=True, ascending=False
+        )
+        df["rank_overdue_days"] = df["overdue_days"].rank(pct=True, ascending=False)
+        df["rank_overall"] = (
+            df.rank_interval
+            + df.rank_revisions
+            + df.rank_average_score
+            + df.rank_latest_score
+            + df.rank_interval_per_revision
+            + df.rank_overdue_days
+        )
+        # Need a unique rank for each item even if there are ties
+        df["risk_rank"] = df["rank_overall"].rank(ascending=False, method="first")
 
-    for index, row in df.iterrows():
-        risk_rank = row["risk_rank"]
-        pages_due[index].update({"risk_rank": int(row["risk_rank"])})
+        for index, row in df.iterrows():
+            risk_rank = row["risk_rank"]
+            pages_due[index].update({"risk_rank": int(row["risk_rank"])})
 
-        # # Group pages into bins of 10 so that they can be tackled together
-        # bin = math.ceil(risk_rank / 10)
-        # # Now order the pages based on page number within each bin; page = index in the data frame
-        # sort_order = round(bin + (index / 1000), 3)
-        # pages_due[index].update({"sort_order": sort_order})
+            # Group pages into bins of 10 so that they can be tackled together
+            bin = math.ceil(risk_rank / 10)
+            # Now order the pages based on page number within each bin; page = index in the data frame
+            sort_order = round(bin + (index / 1000), 3)
+            pages_due[index].update({"sort_order": sort_order})
 
         # Cache this so that revision entry page can automatically move to the next due page
     request.session["pages_due"] = pages_due
@@ -322,7 +326,7 @@ def page_entry(request, student_id, page, due_page):
             # if there are no more due pages, redirect to the main page.
             if pages_due:
                 pages_due_sorted = sorted(
-                    pages_due.items(), key=lambda key_value: key_value[1]["risk_rank"],
+                    pages_due.items(), key=lambda key_value: key_value[1]["sort_order"],
                 )
                 next_page = int(pages_due_sorted[0][0])
 
@@ -334,16 +338,16 @@ def page_entry(request, student_id, page, due_page):
 
     pages_due = request.session.get("pages_due")
 
+    next_page_set = []
     if pages_due:
         pages_due_sorted = sorted(
-            pages_due.items(), key=lambda key_value: key_value[1]["risk_rank"],
+            pages_due.items(), key=lambda key_value: key_value[1]["sort_order"],
         )
 
-        next_page_set = []
-        for i in range(1, 4):
+        for i in range(0, len(pages_due.keys())):
             next_page_set.append(int(pages_due_sorted[i][0]))
 
-        print("next_set=", next_page_set)
+
     return render(
         request,
         "page_entry.html",
@@ -356,6 +360,7 @@ def page_entry(request, student_id, page, due_page):
             "keys_map": keys_map_revision_entry,
             "new_page": new_page,
             "next_page_set": next_page_set,
+            "next_page_set_sorted": sorted(next_page_set),
             "due_page": due_page,
         },
     )
