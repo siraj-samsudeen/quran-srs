ccaab304aa3b512f6327e78085920b7f43ef7cc1
Siraj Samsudeen <mailsiraj@gmail.com>
2020-05-24 12:12:03 +0530

    run dev server on default IP to access it from mobile

--- project/settings_dev.py
+++ project/settings_dev.py
@@ -7,4 +7,5 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "smd1equijn^3ex(6uu)tb1aslfeksymd7&e^wvih!eao0u*x2o"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+# IP address might change on server restart. Hence, better to have a wildcard - 192.168.1.* TODO
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "192.168.1.10"]
