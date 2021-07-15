0b800d2019ae67c817065ae87ca0fb163a5761c8
Siraj Samsudeen <mailsiraj@gmail.com>
2021-07-15 10:40:52 +0530

    unused format command removed

--- core_app/templatetags/custom_format.py
+++ core_app/templatetags/custom_format.py
@@ -5,12 +5,6 @@ register = template.Library()
 
 
 @register.filter
-def format_header(value):
-    # Display  "8.scheduled_due_date" as "Scheduled Due Date"
-    return value.split(".")[-1].replace("_", " ").title()
-
-
-@register.filter
 def display_zero(value):
     return "" if value == 0 else value
 
