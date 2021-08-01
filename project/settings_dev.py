0d990b52c1699c0df4b5f616f4940729f2f70025
Safwan Samsudeen <safwansamsudeen@gmail.com>
2021-08-01 07:07:13 +0530

    installed django-livereload-server to reload static files automatically

--- project/settings_dev.py
+++ project/settings_dev.py
@@ -9,3 +9,28 @@ SECRET_KEY = "smd1equijn^3ex(6uu)tb1aslfeksymd7&e^wvih!eao0u*x2o"
 
 # IP address might change on server restart. Hence, better to have a wildcard - 192.168.1.* TODO
 ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "192.168.1.10"]
+
+# Changes to make live reload of static files - templates and js
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "livereload",
+    "django.contrib.staticfiles",
+    "core_app",
+    "django_extensions",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "livereload.middleware.LiveReloadScript",
+]
