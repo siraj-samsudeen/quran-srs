c9a9a80d44a2ceb2ec62d15e8fd03a95cd3bc204
Siraj Samsudeen <mailsiraj@gmail.com>
2025-02-20 15:37:08 +0530

    fresh start with fasthtml

--- core_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyFirstAppConfig(AppConfig):
-    name = "core_app"
--- static/js/base.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // the update button could be clicked multiple times if the reload is slow
-    // Hence disable it when the form is submitted
-    $('form.card').on('submit', function () {
-        $("button:contains('Update')").prop("disabled", true)
-    })
-})
