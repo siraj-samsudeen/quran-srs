e9eb62d5e7bd8bf2892c972cdff0df7ba4f3c04b
Zaseem-BIsquared <zaseem@bisquared.com>
2025-02-21 11:16:11 +0530

    Add revision page to show the revision history of a page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fasthtml.common import *
+from utils import standardize_column, convert_time
 
 app, rt = fast_app(live=True)
 
@@ -19,11 +20,55 @@ if revisions not in db.t:
         last_modified_at=int,
         pk="id",
     )
+Revision, User = revisions.dataclass(), users.dataclass()
+
+
+column_headers = [
+    "Page",
+    "Revision Time",
+    "Rating",
+    "Created By",
+    "Created At",
+    "Last Modified By",
+    "Last Modified At",
+]
+
+column_standardized = list(map(standardize_column, column_headers))
+date_columns = [
+    c for c in column_standardized if c.endswith("time") or c.endswith("at")
+]
+
+
+def render_revision_row(revision):
+    # Convert the revision object to a dictionary to easily access its attributes by column names
+    rev_dict = vars(revision)
+
+    def render_cell(column, value):
+        if column in date_columns:
+            return Td(convert_time(value))
+        return Td(value)
+
+    return Tr(*[render_cell(c, rev_dict[c]) for c in column_standardized])
 
 
 @rt
 def index():
-    return Titled("Quran SRS Home", Div("Fresh start with FastHTML"))
+    return Titled(
+        "Quran SRS Home",
+        Div("Fresh start with FastHTML"),
+        Div("Nav: ", A("Revision", href=revision)),
+    )
+
+
+@rt
+def revision():
+    table = (
+        Table(
+            Thead(Tr(*map(Th, column_headers))),
+            Tbody(*map(render_revision_row, revisions())),
+        ),
+    )
+    return Titled("Quran SRS Revision", table)
 
 
 serve()
