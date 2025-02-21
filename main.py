4dcc8373482877610e340dd244872fd46b8a2793
Zaseem-BIsquared <zaseem@bisquared.com>
2025-02-21 09:31:37 +0530

    Add database

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from fasthtml.common import *
 
 app, rt = fast_app(live=True)
 
+db = database("data/quran.db")
+
+revisions, users = db.t.revisions, db.t.users
+if revisions not in db.t:
+    users.create(user_id=int, name=str, email=str, password=str, pk="user_id")
+    revisions.create(
+        id=int,
+        user_id=int,
+        page=int,
+        revision_time=int,
+        rating=str,
+        created_by=str,
+        created_at=int,
+        last_modified_by=str,
+        last_modified_at=int,
+        pk="id",
+    )
+
 
 @rt
 def index():
