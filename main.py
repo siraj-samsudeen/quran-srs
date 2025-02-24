d53c2e26cd0d45cd8e7dc2ef142e47c54d8fc143
Zaseem-BIsquared <zaseem@bisquared.com>
2025-02-24 08:09:52 +0530

    Add edit functionality for revisions

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ def render_revision_row(revision):
         hx_get=select.to(id=id),
         target_id=rid,
         hx_swap="outerHTML",
+        id=f"row-{id}",
     )
 
 
@@ -74,18 +75,56 @@ def index():
 
 @rt
 def revision():
-    table = (
-        Table(
-            Thead(Tr(*map(Th, column_headers))),
-            Tbody(*map(render_revision_row, revisions())),
-        ),
+    edit_btn = Button(
+        "Edit", hx_post=edit, hx_target="body", hx_swap="outerHTML", hx_push_url="true"
     )
-    return Titled("Quran SRS Revision", table)
+    actions = Div(edit_btn)
+    table = Table(
+        Thead(Tr(*map(Th, column_headers))),
+        Tbody(*map(render_revision_row, revisions())),
+    )
+    form = Form(actions, table)
+    return Titled("Quran SRS Revision", form)
 
 
 @rt
 def select(id: int):
     return radio_btn(id, True)
+
+
+@app.post
+def edit(revision_id: int):
+    return RedirectResponse(f"/edit?id={revision_id}", status_code=303)
+
+
+@app.get
+def edit(id: int):
+    form = Form(
+        Hidden(name="id"),
+        Label("Page", Input(type="number", name="page")),
+        Label("Date", Input(type="datetime-local", name="revision_time")),
+        Label(
+            "Rating",
+            Select(
+                Option("Good", value="Good"),
+                Option("Ok", value="Ok"),
+                Option("Bad", value="Bad"),
+                name="rating",
+            ),
+        ),
+        Button("Update"),
+        action=update,
+        method="POST",
+    )
+    return Titled("Edit", fill_form(form, revisions[id]))
+
+
+@app.post
+def update(revision: Revision):
+    # Clean up the revision_time
+    revision.revision_time = revision.revision_time.replace("T", " ")
+    revisions.update(revision)
+    return RedirectResponse("/revision", status_code=303)
 
 
 serve()
