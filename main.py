9da1374e882544fa778368f0eefa32728dd33497
Zaseem-BIsquared <zaseem@bisquared.com>
2025-03-05 10:21:06 +0530

    Add edit functionality to the user

--- main.py
+++ main.py
@@ -26,13 +26,40 @@ def index():
 @rt
 def user():
     def _render_user(user):
-        return Tr(Td(user.id), Td(user.name), Td(user.email), Td(user.password))
+        return Tr(
+            Td(user.id),
+            Td(user.name),
+            Td(user.email),
+            Td(user.password),
+            Td(A("Edit", href=f"/user/edit/{user.id}")),
+        )
 
     table = Table(
-        Thead(Tr(Th("ID"), Th("Name"), Th("Email"), Th("Password"))),
+        Thead(Tr(Th("ID"), Th("Name"), Th("Email"), Th("Password"), Th("Action"))),
         Tbody(*map(_render_user, users())),
     )
     return Titled("User", A("Back", href=index), Div(table))
+
+
+@rt("/user/edit/{user_id}")
+def get(user_id: int):
+    user = users[user_id]
+    form = Form(
+        Label("ID", Input(name="id")),
+        Label("Name", Input(name="name")),
+        Label("Email", Input(name="email")),
+        Label("Password", Input(name="password")),
+        Button("Save"),
+        method="POST",
+        action=f"/user/edit/{user_id}",
+    )
+    return Titled("Edit User", fill_form(form, user))
+
+
+@rt("/user/edit/{user_id}")
+def post(user_id: int, user_details: User):
+    users.update(user_details, user_id)
+    return Redirect(user)
 
 
 @rt
