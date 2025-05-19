30b4694d09c1fa620543da92b0b70169e7d5ee11
Zaseem-BIsquared <zaseem@bisquared.com>
2025-05-19 16:08:40 +0530

    test fastmigrate by dropping unwanted columns in hafiz_items table

---
+++ migrations/0002-change-hafiz-items-table.sql
@@ -0,0 +1,6 @@
+ALTER TABLE hafizs_items DROP COLUMN revision_count;
+ALTER TABLE hafizs_items DROP COLUMN last_revision_date;
+ALTER TABLE hafizs_items DROP COLUMN last_revision_rating;
+ALTER TABLE hafizs_items DROP COLUMN total_score;
+ALTER TABLE hafizs_items DROP COLUMN good_streak;
+ALTER TABLE hafizs_items DROP COLUMN bad_streak;
