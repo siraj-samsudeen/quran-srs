8a0792e30c42aee24d4cd62e230ffa4a382bd2c1
Zaseem-BIsquared <zaseem@bisquared.com>
2025-07-04 08:01:53 +0530

    refactor: Update the db schema to add a new column on revisions tables #201

---
+++ migrations/0008-create-srs-booster-pack-and-changes-on-hafizs_items_and_revisions.sql
@@ -0,0 +1,20 @@
+CREATE TABLE srs_booster_pack (
+    id                INTEGER PRIMARY KEY,
+    name              TEXT,
+    description       TEXT,
+    min_interval      INTEGER,
+    max_interval      INTEGER,
+    interval_days     TEXT
+);
+
+
+-- Changes on the hafizs_items table
+ALTER TABLE hafizs_items ADD COLUMN srs_booster_pack_id INTEGER REFERENCES srs_booster_pack(id);
+ALTER TABLE hafizs_items ADD COLUMN good_streak INTEGER;
+ALTER TABLE hafizs_items ADD COLUMN bad_streak INTEGER;
+ALTER TABLE hafizs_items ADD COLUMN next_interval INTEGER;
+
+--Changes on revisions table'
+ALTER TABLE revisions ADD COLUMN planned_interval INTEGER;
+ALTER TABLE revisions ADD COLUMN actual_interval INTEGER;
+ALTER TABLE revisions ADD COLUMN next_interval INTEGER;
