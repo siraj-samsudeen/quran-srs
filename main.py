186646ce1a6b89ab3f54d95298dd7290c10fc992
Zaseem-BIsquared <zaseem@bisquared.com>
2025-07-04 16:20:02 +0530

    refactor: cleanup the current SRS table #201

--- main.py
+++ main.py
@@ -4894,47 +4894,14 @@ def srs_detailed_page_view(auth):
         else:
             # This block mostly never run unless we don't have items details in the hafizs_items table
             next_review = None
-        is_due = calculate_days_difference(next_review, current_date) >= 0
-        return Tr(
-            Td(get_page_description(item_id)),
-            Td(next_review),
-            *(
-                (Td(), Td())
-                if not is_due
-                else (
-                    Td(
-                        Div(
-                            rating_dropdown(
-                                default_rating=None,
-                                is_label=False,
-                                cls="[&>div]:h-8 uk-form-sm w-28",
-                            ),
-                            cls="max-w-28",
-                        )
-                    ),
-                    Td(
-                        Button(
-                            "Record",
-                            hx_post=f"srs/{item_id}",
-                            hx_include="closest tr",
-                            hx_swap="none",
-                        )
-                    ),
-                )
-            ),
-        )
+        return Tr(Td(get_page_description(item_id)), Td(next_review))
 
     current_srs_table = Div(
         H4("SRS Pages"),
         Div(
             Table(
                 Thead(
-                    Tr(
-                        Td("Page"),
-                        Td("Next Review"),
-                        Td("Rating"),
-                        Td(""),
-                    ),
+                    Tr(Td("Page"), Td("Next Review")),
                     cls="sticky z-50 top-0 bg-white",
                 ),
                 Tbody(*map(render_current_srs_rows, current_srs_items)),
@@ -4957,11 +4924,6 @@ def get_srs_next_interval(pack_id: int, current_interval: int):
     booster_pack_intervals = booster_pack_details.interval_days.split(",")
     booster_pack_intervals = list(map(int, booster_pack_intervals))
     return find_next_greater(booster_pack_intervals, current_interval)
-
-
-@app.post("/srs/{item_id}")
-def add_srs_records(item_id: int, rating: int):
-    pass
 
 
 @app.get("/start-srs/{item_id}")
